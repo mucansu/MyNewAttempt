88375580006670bcd45788bea02060366842704b
mucansu <51933067+mucansu@users.noreply.github.com>
2022-11-09 14:11:23 +0100

    MyJava.java

---
+++ MyJava.java
@@ -0,0 +1,17 @@
+public class Items {
+    public static void main(String[] args) {
+        myItems("mustafa","cansu");
+        myAge(26);
+    }
+
+    public static void myItems (String a,String b){
+        System.out.println(a + " - " + b);
+
+    }
+    public static int myAge (int c){
+        System.out.println(c);
+
+        return c;
+    }
+
+}
